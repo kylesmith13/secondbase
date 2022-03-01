6d3b3cb198c325462a262af51c538cafba266051
Kyle Smith <30356483+kylesmith13@users.noreply.github.com>
2022-03-01 12:10:23 -0600

    Update on_base.rb

--- lib/second_base/on_base.rb
+++ lib/second_base/on_base.rb
@@ -13,7 +13,7 @@ module SecondBase
     original_configurations = Rails.application.config.database_configuration
     original_migrations_path = ActiveRecord::Tasks::DatabaseTasks.migrations_paths
     original_db_dir = ActiveRecord::Tasks::DatabaseTasks.db_dir
-    ActiveRecord::Tasks::DatabaseTasks.current_config = config
+#     ActiveRecord::Tasks::DatabaseTasks.current_config = config
     ActiveRecord::Base.configurations = original_configurations[Railtie.config_key]
     ActiveRecord::Base.establish_connection(config)
     ActiveRecord::Tasks::DatabaseTasks.migrations_paths = [SecondBase::Railtie.fullpath('migrate')]
